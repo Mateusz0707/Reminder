f9601b4369bfd499d0b5d2be92606f181fef5cdd
Mateusz0707 <matfijalkowski123@gmail.com>
2024-03-31 17:58:29 +0200

    zmiana

--- index.php
+++ index.php
@@ -22,18 +22,18 @@
 
         <div class="fixed top-0 right-0 p-1 flex space-x-2">
             <button
-                class="bg-transparent text-right   py-2 px-1 rounded focus:outline-none focus:ring-2 focus:ring-green-600"
+                class="bg-transparent text-right hover:bg-gray-300   py-2 px-1 rounded focus:outline-none focus:ring-2 focus:ring-green-600"
                 id="openButton" onclick="openPopup()">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
             </button>
-            <button class="bg-transparent text-right rounded focus:outline-none ">
+            <button class="bg-transparent hover:bg-gray-300 px-2 text-right rounded focus:outline-none ">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
                 </svg>
             </button>
-            <button class="bg-transparent text-right rounded focus:outline-none ">
+            <button class="bg-transparent hover:bg-gray-300 text-right rounded focus:outline-none ">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 </svg>
